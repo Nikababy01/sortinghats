89a93fdabc24fde9396dc4014295f0c1e3e57849
Monique.Bass01 <monique.bass01@gmail.com>
2020-02-01 13:38:30 -0600

    added new names

--- main.js
+++ main.js
@@ -1,28 +1,49 @@
-const studentProfile = [
 
+const students=[];
 
-    
-]
-
-const printToDom = (divId,textToPrint) => {
+const printToDom = (divId, textToPrint) => {
     const selectedDiv= document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
+};
+
+const buildStudentCard= (mystudent)=> {
+    let domString = '';
+    for(let i = 0; i < mystudent.length; i++){
+        domString += '<div id="housecards" class="card" style="width: 18rem;">';
+        domString += '<div class="card-body">';
+        domString += `<h5 class="card-title" id="name">${mystudent[i].name}</h5>`;
+        domString += `<p class="card-text" id="house">${mystudent[i].house}</p>`;
+        domString += `<a href="#" class="btn btn-primary">Expel</a>`;
+        domString += '</div>';
+        domString += '</div>';
+    }
+ printToDom('studentHouse',domString);
+};
+const sortStudent = (e)=>{
+    e.preventDefault()
+    console.log(e)
     
+    const student={
+        id: 'All',  //generate id
+        name:'NAMES',  //get from input
+        house:'HOUSES'  //random sort
+    }
+    students.push(student)
+    buildStudentCard(students);
+}
+
     
-    const buildHarry= (students)=> {
-        let domString = '';
-        for(let i = 0; i < students.length; i++){
-            domString+= '<div class="col-md-6 col-lg-4 card-separation">';
-            domString+= '<div class="card">';
-            domString+=`<img src="${quacks[i].imageUrl}" class="card-img-top" alt="...">`;
-            domString +='<div class="card-body">';
-            domString+=`<h5 class="card-title">${quacks[i].name}</h5>`;
-            domString+=`<p class="card-text">${quacks[i].socialStatus}</p>`;
-            domString+=`<p class="card-text">${quacks[i].diet}</p>`;
-            domString+= '</div>';
-            domString+= '</div>';
-            domString+= '</div>';
+ 
+const events = ()=> {
+    document.getElementById('sorthouse').addEventListener('click',sortStudent);
+};
     
-        }
-        printToDom('ducks',domString);
-    };
+
+
+ const init =()=> {
+    events();
+  }
+ init();
+
+
+ 
