d74f8ab37c1623bc7424258209ea82b84f8e3307
Monique.Bass01 <monique.bass01@gmail.com>
2020-02-09 19:27:55 -0600

    removed console.log

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 const students=[];
 const studentHouse = ["Gryffindor","Hufflepuff", "Ravenclaw","Slytherin"];
+const expeledStudents=[];
+
 
 const printToDom = (divId, textToPrint) => {
     const selectedDiv= document.getElementById(divId);
@@ -9,41 +11,42 @@ const printToDom = (divId, textToPrint) => {
 
 const buildStudentCard= (mystudent)=> {
     let domString = '';
-    for(let i = 0; i < mystudent.length; i++){
-    
-        domString += '<div id="housecards" class="card" style="width: 18rem;">';
-        domString += `<div class="card-body ${mystudent[i].house}">`;
-        domString += `<h5 class="card-title" id="name">${mystudent[i].name}</h5>`;
-        domString += `<p class="card-text" id="house">${mystudent[i].house}</p>`;
-        domString += `<button id="remove" onclick= "btnexpel(${mystudent[i].id})" class="btn btn-primary">Expel</button>`;
-        domString += '</div>';
-        domString += '</div>';
-    }
+        for(let i = 0; i < mystudent.length; i++){
+            domString += '<div id="housecards" class="card" style="width: 18rem;">';
+            domString += `<div class="card-body ${mystudent[i].house}">`;
+            domString += `<h5 class="card-title" id="name">${mystudent[i].name}</h5>`;
+            domString += `<p class="card-text" id="house">${mystudent[i].house}</p>`;
+            domString += `<button id="remove" onclick= "btnexpel(${mystudent[i].id})" class="btn btn-primary">Expel</button>`;
+            domString += '</div>';
+            domString += '</div>';
+        }
  printToDom('studentHouse',domString);
 };
 
-const btnexpel= (id)=> {
-    console.log(`button expel clicked ${id}`);
-    for(let i = 0; i < students.length; i++) {
+    const btnexpel= (id)=> {
+       for(let i = 0; i < students.length; i++) {
         if(students[i].id === id) {
-            students.splice(i, 1);//defines the parameter
+            expeledStudents.push( students.splice(i, 1));//defines the parameter
             document.getElementById("housecards").innerHTML="";
             buildStudentCard(students);
+           
          }
     }
-console.log(students);
+
 };
+
+
 
 let cnt = 0;
 const uniqueId= ()=>{
-    cnt= cnt + 1;
-    console.log(cnt);
-    return cnt;
+cnt= cnt + 1;
+ return cnt;
+
 };
 
 const sortStudent = (e)=>{
     e.preventDefault()
-    console.log(e)
+    
     // before newStudent only pertains to this object
     const newStudent={
         id: uniqueId(),  //generate id
@@ -52,25 +55,18 @@ const sortStudent = (e)=>{
     }
         students.push(newStudent)
         buildStudentCard(students);
-        document.getElementById("name").value="";
-        
+        document.getElementById("name").value="";  
 
-}
-
-
-
+};
 
 const events = ()=> {
     document.getElementById('sorthouse').addEventListener('click',sortStudent);
 };
-    
 
-
- const init =()=> {
+const init =()=> {
     events();
-    
-    
-  }
+
+     }
  init();
 
 
